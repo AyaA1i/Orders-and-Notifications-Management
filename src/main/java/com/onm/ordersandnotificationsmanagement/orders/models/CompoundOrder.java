9655a20bc329c911a77a98e0c469efb91cfdecba
ayaaalli <ayaali95248@gmail.com>
2023-12-26 09:37:10 +0200

    trying to merge orders with notifications

--- src/main/java/com/onm/ordersandnotificationsmanagement/orders/models/CompoundOrder.java
+++
@@ -1,9 +0,0 @@
-package com.onm.ordersandnotificationsmanagement.orders.models;
-
-import com.onm.ordersandnotificationsmanagement.orders.models.Order;
-
-public class CompoundOrder extends Order{
-    public CompoundOrder(int id) {
-        this.setId(id);
-    }
-}
